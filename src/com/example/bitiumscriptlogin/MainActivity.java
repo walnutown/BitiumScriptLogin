11feae59a9d3b19358348ef4d4d270811ceee5fa
Tao Hu <walnutown@yahoo.com>
2014-04-04 17:56:16 -0700

    final

--- src/com/example/bitiumscriptlogin/MainActivity.java
+++ src/com/example/bitiumscriptlogin/MainActivity.java
@@ -31,7 +31,8 @@ public class MainActivity extends Activity {
 	// private final static String lucidChartPollingUrl =
 	// "http://bitium.192.168.1.59.xip.io/api/v1/2/subscriptions/4/auth?plugin=true&copernicus=false&launch_id=30BF4E12-CBA8-4363-B51A-70C3E0351160&token=xx-WGhLQx_QFFX_E5rpb";
 	private final static String lucidChartPollingUrl = "https://www.bitium.com/api/v1/1/subscriptions/57756/auth?plugin=true&copernicus=true&launch_id=ebabe2c2-9c14-4108-b188-c8d8f299f0e1&token=BWJnpS5LoRxZ9GvShshM";
-	private final static String githubPollingUrl = "http://bitium.192.168.1.59.xip.io/api/v1/2/subscriptions/2/auth?plugin=true&copernicus=false&launch_id=F4A3C95D-1426-4349-B340-06689AFBDD17&token=xx-WGhLQx_QFFX_E5rpb";
+	//private final static String githubPollingUrl = "http://bitium.192.168.1.45.xip.io/api/v1/2/subscriptions/2/auth?plugin=true&copernicus=false&launch_id=F4A3C95D-1426-4349-B340-06689AFBDD17&token=xx-WGhLQx_QFFX_E5rpb";
+	private final static String githubPollingUrl =  "https://www.bitium.com/api/v1/1/subscriptions/36518/auth?plugin=true&copernicus=true&launch_id=e3c178f8-5912-4a19-9fb6-2bbc3c97c1ef&token=BWJnpS5LoRxZ9GvShshM";
 	private ScriptLoginManager scriptLoginManager;
 	private FileManager fileManager;
 
@@ -50,7 +51,8 @@ public class MainActivity extends Activity {
 		wv.setWebViewClient(new ScriptLoginWebViewClient());
 		wv.setWebChromeClient(new ScriptLoginWebChromeClient());
 		try {
-			JSONObject json_actions = new HttpGetJsonTask(lucidChartPollingUrl).execute().get();
+			//JSONObject json_actions = new HttpGetJsonTask(lucidChartPollingUrl).execute().get();
+			JSONObject json_actions = new HttpGetJsonTask(githubPollingUrl).execute().get();
 			// ScriptLoginAction[] scriptLoginActions =
 			// JSONManager.getScriptLoginActionsFromJSON(json_actions);
 			// Log.d("SCRIPT", Arrays.toString(scriptLoginActions));
@@ -128,9 +130,9 @@ public class MainActivity extends Activity {
 		public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
 			String msg = consoleMessage.message();
 			Log.d("Console", msg + " -- lineNumber: " + consoleMessage.lineNumber());
-			if (msg.equals("login_success"))
+			if (msg.equals("Android:login_success"))
 				return true;
-			else if (msg.startsWith("action") || msg.startsWith("check")) {
+			else if (msg.startsWith("Android:action") || msg.startsWith("Android:check")) {
 				try {
 					scriptLoginManager.runAction(msg);
 				} catch (JSONException e) {
--- src/com/example/bitiumscriptlogin/ScriptLoginManager.java
+++ src/com/example/bitiumscriptlogin/ScriptLoginManager.java
@@ -60,42 +60,34 @@ public class ScriptLoginManager {
 	 */
 	public void loadJavaScript() throws IOException {
 		Log.d("SCRIPT", "DOM ready, load our own JS");
+		loadJavaScriptFiles();
 		StringBuilder js = new StringBuilder();
-		// js.append("window.console= console;\n");
 		js.append("window.__bitium = new function(){\n");
-		js.append("     	console.log('JS, 1.1');\n");
-		// js.append(loadJavaScriptFiles());
-		js.append(fileManager.readAssetFile("js/jquery.min.js"));
-//		js.append("     console.log('load jquery.min.js');\n");
-//		js.append(fileManager.readAssetFile("js/jquery.simulate.js"));
-//		js.append(fileManager.readAssetFile("js/jquery.simulate.ext.js"));
-//		js.append(fileManager.readAssetFile("js/jquery.simulate.drag-n-drop.js"));
-//		js.append(fileManager.readAssetFile("js/jquery.simulate.key-combo.js"));
-		js.append(fileManager.readAssetFile("js/jquery.simulate.key-sequence.js"));
-		js.append(fileManager.readAssetFile("js/bililiteRange.js"));
-		js.append(fileManager.readAssetFile("js/page.js"));
-		js.append("     	console.log('JS, 1.2');\n");
+		js.append("     	console.log('JS, 1');\n");
 		js.append("		$ = jQuery.noConflict();\n");
-		js.append("     	console.log('JS, 1.3');\n");
-//		js.append("  	var page = new window.__bitium_Page();\n");
-//		js.append("     	console.log('JS, 1.4');\n");
+		js.append("     	console.log('JS, 1.1');\n");
+		js.append("  	var page = new window.__bitium_Page();\n");	
+		js.append("     	console.log('JS, 1.2');\n");
 		js.append("  	this.run_step = function(step, last_step, callback) {\n");
-		js.append("     	console.log('JS, 1.5');\n");
+		js.append("     	console.log('JS, 2');\n");
 		js.append("     page.last_input = last_step;\n");
-		js.append("     	console.log('JS, 1.6');\n");
+		js.append("     	console.log('JS, 2.1');\n");
 		js.append("     page.run_step(step, callback);\n");
-		js.append("     	console.log('JS, 1.7');\n");
+		js.append("     	console.log('JS, 2.2');\n");
 		js.append("     }\n");
 		js.append("};\n");
-		js.append("console.log('action:true');\n");
-		executeJavaScript(js.toString());
+		js.append("window.bitiumDateNow = function(){\n");
+		js.append("		d = new Date();\n");
+		js.append("    	return d.getTime();\n");
+		js.append("}\n");
+		js.append("console.log('Android:action;true');\n");
+		executeJavaScriptWithLog(js.toString());
 
 	}
 
-	int lineCount = 1;
-
+	
 	private void logByLine(String s) {
-
+		int lineCount = 1;
 		for (String line : s.split("\n")) {
 			Log.d("JS", "line" + lineCount + " " + line);
 			lineCount++;
@@ -112,23 +104,19 @@ public class ScriptLoginManager {
 		}
 	}
 
-	private void executeJavaScript(String js) {
+	private void executeJavaScriptWithLog(String js) {
 
 		StringBuilder sb = new StringBuilder();
 		sb.append("try{ \n");
 		sb.append(js);
 		sb.append("}catch(e){\n");
-		// sb.append("if (console === undefined){\n");
-		//sb.append("	  console.trace();\n");
 		sb.append("	  console.log(e.stack);\n");
-//		 sb.append("	  document.write(e);\n");
 		sb.append("}\n");
 		logByLine(sb.toString());
-//		logAll(sb.toString());
 		webView.loadUrl("javascript:" + sb.toString());
 	}
 	
-	private void executeJavaScript2(String js) {
+	private void executeJavaScript(String js) {
 		webView.loadUrl("javascript:(" + js+")");
 	}
 
@@ -139,16 +127,18 @@ public class ScriptLoginManager {
 	 * @throws JSONException
 	 */
 	public void runAction(String msg) throws JSONException {
+		Log.d("SCRIPT", "runAction() " + msg);
 		String previousActionJS = "undefined", actionJS = "";
-		String[] s = msg.split(":");
-		String method = s[0], result = s[1];
+		String[] s = msg.split(";");
+		String method = s[0].substring(8), result = s[1];
 		if (method.equals("action")) {
 			previousActionJS = jsonActions.getJSONObject(jsonActions.length() - 2).toString();
 			actionJS = jsonActions.getJSONObject(jsonActions.length() - 1).toString();
 			method = "check";
 		} else if (method.equals("check")) {
 			if (result.equals("true")) {
-				executeJavaScript("console.log('login_success')");
+				executeJavaScript("console.log('Android:login_success');");
+				Log.d("SCRIPT", "login success");
 				return;
 			} else {
 				if (actionIndex == jsonActions.length() - 1) {
@@ -161,29 +151,37 @@ public class ScriptLoginManager {
 				}
 			}
 		}
+		Log.d("SCRIPT", "actionJS: " + actionJS);
 		StringBuilder js = new StringBuilder();
+		js.append("     	console.log('JS, 3');\n");
 		js.append("var step =(" + actionJS + ");\n");
+		js.append("     	console.log('JS, 3.1');\n");
 		js.append("var last_step=(" + previousActionJS + ");\n");
+		js.append("     	console.log('JS, 3.2');\n");
 		js.append("__bitium.run_step(step, last_step, function(result) {\n");
-		js.append("		console.log('" + method + ":'+ result);\n");
+		js.append("			console.log('Android:" + method + ";'+ result);\n");
 		js.append("});\n");
-		executeJavaScript(js.toString());
+		executeJavaScriptWithLog(js.toString());
 	}
 
 	/**
 	 * Load Javascript files that need to be injected into the webview
 	 */
-	private String loadJavaScriptFiles() throws IOException {
+	private void loadJavaScriptFiles() throws IOException {
 		StringBuilder sb = new StringBuilder();
 		String[] files = getJavaScriptFilesToBeLoaded();
 		for (String file : files) {
-			sb.append(fileManager.readAssetFile(file));
-			sb.append("console.log('load '" + file + "');");
+			sb.delete(0, sb.length());
+			sb.append(fileManager.readAssetFile(file)+"\n");
+			sb.append("console.log('load " + file + "');\n");
+			executeJavaScriptWithLog(sb.toString());
 		}
 		// TODO: we use local page.js now, it should be replaced with the file
-		// on the server
-		sb.append(fileManager.readAssetFile("js/page.js"));
-		return sb.toString();
+		// downloaded from on the server
+		sb.delete(0, sb.length());
+		sb.append(fileManager.readAssetFile("js/page.js")+"\n");
+		sb.append("console.log('load js/page.js');\n");
+		executeJavaScriptWithLog(sb.toString());
 	}
 
 	private String[] getJavaScriptFilesToBeLoaded() {
@@ -193,8 +191,8 @@ public class ScriptLoginManager {
 		files[2] = "js/jquery.simulate.ext.js";
 		files[3] = "js/jquery.simulate.drag-n-drop.js";
 		files[4] = "js/jquery.simulate.key-combo.js";
-		files[5] = "js/jquery.simulate.key-sequence.js";
-		files[6] = "js/bililiteRange.js";
+		files[5] = "js/bililiteRange.js";
+		files[6] = "js/jquery.simulate.key-sequence.js";
 		return files;
 	}
 
